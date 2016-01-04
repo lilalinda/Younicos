f7a01f36e3d7c1720e48f6f8939359968433ca9a
Linda Briesemeister <linda.briesemeister@gmx.de>
2016-01-04 11:35:07 +0100

    adding command line parsing for options and arguments new README

---
+++ README.md
@@ -0,0 +1,20 @@
+Younicos Programming Task
+=========================
+
+This Java project implements a Younicos programming task.  
+
+
+Requirements
+------------
+
+* Java 7 or higher
+* Maven 3
+
+
+Quickstart
+-----------
+
+$> mvn package
+$> java -jar target/Younicos-<...>-jar-with-dependencies.jar -h
+$> java -jar target/Younicos-<...>-jar-with-dependencies.jar -m 6 -f input-XML-file
+
--- src/main/java/com/lilalinda/younicos/Main.java
+++ src/main/java/com/lilalinda/younicos/Main.java
@@ -1,8 +1,42 @@
 package com.lilalinda.younicos;
 
-public class Main {
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.PrintStream;
+
+public final class Main {
+
+    private static void printUsage(PrintStream out, CmdLineParser parser) {
+        out.println("usage: java -jar Younicos-<...>-jar-with-dependencies.jar [options...]");
+        out.println("with");
+        parser.printUsage(out);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        // parse arguments
+        YounicosOptions options = new YounicosOptions();
+        CmdLineParser parser = new CmdLineParser(options);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            printUsage(System.err, parser);
+            return;
+        }
+
+        if (options.displayHelp) {
+            printUsage(System.out, parser);
+            return;
+        }
+
+        // TODO: run validation
+        System.out.println("Running validation");
+    }
+
+    static class YounicosOptions {
+        @Option(name="-h",usage="display usage and exit")
+        boolean displayHelp = false;
     }
 }
