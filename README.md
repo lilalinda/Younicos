7b1de22f45c89354a9f8f701f6a1a4908ae02874
Linda Briesemeister <linda.briesemeister@gmx.de>
2016-01-04 14:22:04 +0100

    finishing command line parsing

--- README.md
+++ README.md
@@ -3,18 +3,17 @@ Younicos Programming Task
 
 This Java project implements a Younicos programming task.  
 
-
 Requirements
 ------------
 
 * Java 7 or higher
 * Maven 3
 
-
 Quickstart
 -----------
 
-$> mvn package
-$> java -jar target/Younicos-<...>-jar-with-dependencies.jar -h
-$> java -jar target/Younicos-<...>-jar-with-dependencies.jar -m 6 -f input-XML-file
+    $> mvn package
+    $> java -jar target/Younicos-<...>-jar-with-dependencies.jar -h
+    $> java -jar target/Younicos-<...>-jar-with-dependencies.jar -m 6 -f src/test/resources/Profile1.xml
+
 
--- src/main/java/com/lilalinda/younicos/Main.java
+++ src/main/java/com/lilalinda/younicos/Main.java
@@ -4,6 +4,7 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import java.io.File;
 import java.io.PrintStream;
 
 public final class Main {
@@ -31,6 +32,18 @@ public final class Main {
             return;
         }
 
+        // check other settings:
+        if (options.maxPower == 0) {
+            System.err.println("Need maximum power other than 0!");
+            printUsage(System.err, parser);
+            return;
+        }
+        if (options.inputFile == null || !options.inputFile.canRead() || !options.inputFile.isFile()) {
+            System.err.println("Cannot open input XML file to read!");
+            printUsage(System.err, parser);
+            return;
+        }
+
         // TODO: run validation
         System.out.println("Running validation");
     }
@@ -38,5 +51,11 @@ public final class Main {
     static class YounicosOptions {
         @Option(name="-h",usage="display usage and exit")
         boolean displayHelp = false;
+
+        @Option(name="-m",metaVar="MAX",usage="max. power to charge and discharge (in MW)")
+        float maxPower = 0;
+
+        @Option(name="-f",usage="XML file to validate")
+        File inputFile;
     }
 }
